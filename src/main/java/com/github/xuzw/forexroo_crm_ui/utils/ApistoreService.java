0e6da74cbaffdf4ddf8f77dcf8831d0aada56d1b
xuzewei <xuzewei_2012@126.com>
2017-06-27 14:35:58 +0800

    ApistoreService.java

--- src/main/java/com/github/xuzw/forexroo_crm_ui/utils/ApistoreService.java
+++ src/main/java/com/github/xuzw/forexroo_crm_ui/utils/ApistoreService.java
@@ -13,15 +13,19 @@ import com.jcabi.http.request.JdkRequest;
  * @time 2017年6月7日 上午11:44:53
  */
 public class ApistoreService {
-    public static final String key = "23ea02288ae1b784c17de701d77cda4c";
-
     public static JSONObject sendSms(String mobile, SmsTemplateEnum smsTemplateEnum, JSONObject tplArgs) throws IOException {
         Map<String, String> params = new HashMap<>();
-        params.put("key", ApistoreService.key);
+        params.put("key", "23ea02288ae1b784c17de701d77cda4c");
         params.put("mobile", mobile);
         params.put("tpl_id", String.valueOf(smsTemplateEnum.getTemplateId()));
-        params.put("tpl_val", tplArgs.toJSONString());
+        if (null != tplArgs) {
+            params.put("tpl_val", tplArgs.toJSONString());
+        }
         String json = new JdkRequest("http://v.apistore.cn/api/v14/xsend").uri().queryParams(params).back().method(com.jcabi.http.Request.GET).fetch().body();
         return JSON.parseObject(json);
+    }
+
+    public static JSONObject sendSms(String mobile, SmsTemplateEnum smsTemplateEnum) throws IOException {
+        return sendSms(mobile, smsTemplateEnum, null);
     }
 }
